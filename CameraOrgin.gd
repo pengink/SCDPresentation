4554947b45b4eac6938768fd3098187517727fb0
pengink <150305134+pengink@users.noreply.github.com>
2024-05-11 02:33:59 -0700

    vessel and improved camera
    blur_false

--- CameraOrgin.gd
+++ CameraOrgin.gd
@@ -1,8 +1,9 @@
 extends Spatial
 
 
-var sens : float = 0.01
-onready var scroll = get_node("Camera/Scroll")
+var sens : float = 0.002
+onready var scroll = get_node("ClippedCamera/Scroll")
+var toRot : Vector3
 
 var past : bool = false
 
@@ -10,15 +11,18 @@ func _ready():
 	pass
 	
 func _process(delta):
-	if scroll.scrollPos >= -10 && past == false:
-		print("past")
+	if scroll.scrollPos <= -10 && past == false:
+		print_debug("past")
 		past = true
 	elif scroll.scrollPos <= 10:
-		get_node("Camera").translation.z = -4
+		#get_node("ClippedCamera").translation.z = -4
+		pass
+	transform.basis = transform.basis.slerp(Basis(toRot), 0.02)
 
 func _input(event):
 	if event is InputEventMouseMotion:
 		if Input.is_action_pressed("click"):
-			rotation.y -= event.relative.x * sens
-			rotation.x -= event.relative.y * sens
-			rotation_degrees.x = clamp(rotation_degrees.x, -75, 75)
+			toRot.y -= event.relative.x * sens
+			toRot.x -= event.relative.y * sens
+			toRot.x = clamp(toRot.x, deg2rad(-60), deg2rad(60))
+			toRot.y = clamp(toRot.y, deg2rad(-45), deg2rad(45))
